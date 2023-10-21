c6878861102e56128c0bebbdc97a2c3a2908df64
Halil Ücel <hucelsoft@gmail.com>
2023-10-21 10:39:27 +0300

    ssa

--- lib/sources/life_cycle/web_project_lifecycle.dart
+++ lib/sources/life_cycle/web_project_lifecycle.dart
@@ -1,5 +1,5 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+//import 'dart:html' as html;
+import 'package:universal_html/html.dart' as html;
 
 class WebLifeCycleObserver {
   static WebLifeCycleObserver? _of;
